5fc5931955167485392e8a454bcaa13f07285201
mumuy <zhenghaole1990@126.com>
2016-09-27 10:24:52 +0800

    updated 添加部分姑表关系

--- tests/test.js
+++ tests/test.js
@@ -7,8 +7,8 @@ test('relationship.js show to be tested', function (t) {
   t.deepEqual(relationship({text:'儿子的爸爸的妈妈',sex:1}),['妈妈']);
   t.deepEqual(relationship({text:'老婆的老公',sex:1}),['自己']);
   t.deepEqual(relationship({text:'老婆的外孙的姥爷',sex:1}),['自己']);
-  t.deepEqual(relationship({text:'表姐的哥哥',sex:1}),['表哥(姑家)','表哥(舅家)','表哥(姨家)']);
-  t.deepEqual(relationship({text:'表姐的妹妹',sex:1}),['表姐(姑家)','表妹(姑家)','表姐(舅家)','表妹(舅家)','表姐(姨家)','表妹(姨家)']);
+  t.deepEqual(relationship({text:'表姐的哥哥',sex:1}),['姑表哥','舅表哥','姨表哥']);
+  t.deepEqual(relationship({text:'表姐的妹妹',sex:1}),['姑表姐','姑表妹','舅表姐','舅表妹','姨表姐','姨表妹']);
   t.deepEqual(relationship({text:'老公的老婆的儿子的爸爸',sex:0}),['老公']);
   t.deepEqual(relationship({text:'爸爸的舅舅',sex:0,reverse:true}),['外甥孙女']);
   t.deepEqual(relationship({text:'舅爷爷',type:'chain'}),['爸爸的妈妈的兄弟']);
