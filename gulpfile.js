1164f70c897051f3f1dcc99e0eb8692a2afa7070
mumuy <zhenghaole@gmail.com>
2022-02-28 17:16:34 +0800

    updated 压缩文件保留注释

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,13 @@ const rename = require("gulp-rename");
 
 gulp.task('mini', () => (
   gulp.src('script/relationship.js')
-  .pipe(uglify())
+  .pipe(uglify({
+    output:{
+      comments: function(node, comment){
+        return /^!/.test(comment.value);
+      }
+    }
+  }))
   .pipe(rename("relationship.min.js"))
   .pipe(gulp.dest('dist/'))
   
