14d9284601cf841f94800a7eab506e920af557c5
PASSER-BY <zhenghaole1990@126.com>
2022-03-16 23:54:01 +0800

    updated 添加繁体支持

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,25 @@ const fs = require('fs');
 const gulp = require('gulp');
 const uglify = require('gulp-uglify');
 const rename = require("gulp-rename");
+const cht = require('gulp-cht');
 
-gulp.task('mini', () => (
-  gulp.src('script/relationship.js')
-  .pipe(uglify({
-    output:{
-      comments: function(node, comment){
-        return /^!/.test(comment.value);
-      }
-    }
-  }))
-  .pipe(rename("relationship.min.js"))
-  .pipe(gulp.dest('dist/'))
-  
-));
+gulp.task('mini', () => {
+    // gulp.src('./index.html')
+    // .pipe(cht())
+    // .pipe(rename("zh-HK.html"))
+    // .pipe(gulp.dest("./"));
+
+    gulp.src('script/relationship.js')
+    .pipe(uglify({
+        output:{
+            comments: function(node, comment){
+                return /^!/.test(comment.value);
+            }
+        }
+    }))
+    .pipe(rename("relationship.min.js"))
+    .pipe(gulp.dest('dist/'))
+    .pipe(cht())
+    .pipe(rename("relationship.zh-HK.min.js"))
+    .pipe(gulp.dest('dist/'))
+});
