99ca69948bf6fd784ed6914e411151597d12504d
PASSER-BY <zhenghaole1990@126.com>
2023-01-30 22:25:16 +0800

    updated  小类称呼统一判断

--- src/module/selector.js
+++ src/module/selector.js
@@ -123,8 +123,8 @@ export function getSelectors(str){
         getList(name);
         // 通过关键词找关系
         keywords.forEach(function(name){
-            let match = name.match(/^[大|小]|^[一|二|三|四|五|六|七|八|九|十]+/);
-            if(match){
+            let match = name.replace(/^[尕幺细满碎晚末尾幼]/,'小').match(/^[大|小]|^[一|二|三|四|五|六|七|八|九|十]+/);
+            if(match){  // 匹配排序
                 let x_name = name.replace(match[0],'几');
                 let r_name = name.replace(match[0],'');
                 let num = zh2number(match[0]);
