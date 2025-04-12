a706d387cc391f4a39e9deb3447d07d59556e86c
mumuy <zhenghaole1990@126.com>
2025-04-12 12:35:28 +0800

    updated js优化

--- src/relationship.js
+++ src/relationship.js
@@ -4,44 +4,39 @@ import {reverseId,getItemsById,getChainById,getPairsById} from './module/id.js';
 import {setModeData,getModeData,modeData} from './module/mode.js';
 
 // 对外方法
-let relationship = function (parameter){
+let relationship = function (parameter) {
     if(typeof parameter =='string'){
         parameter = getOptions(parameter);
     }
     let options = Object.assign({
-        text:'',            // 目标对象：目标对象的称谓汉字表达，称谓间用‘的’字分隔
-        target:'',          // 相对对象：相对对象的称谓汉字表达，称谓间用‘的’字分隔，空表示自己
-        sex:-1,             // 本人性别：0表示女性,1表示男性
-        type:'default',     // 转换类型：'default'计算称谓,'chain'计算关系链,'pair'计算关系合称
-        reverse:false,      // 称呼方式：true对方称呼我,false我称呼对方
-        mode:'default',     // 模式选择：使用setMode方法定制不同地区模式，在此选择自定义模式
-        optimal:false       // 最短关系：计算两者之间的最短关系
-    },parameter);
+        text: '',            // 目标对象：目标对象的称谓汉字表达，称谓间用‘的’字分隔
+        target: '',          // 相对对象：相对对象的称谓汉字表达，称谓间用‘的’字分隔，空表示自己
+        sex: -1,             // 本人性别：0表示女性,1表示男性
+        type: 'default',     // 转换类型：'default'计算称谓,'chain'计算关系链,'pair'计算关系合称
+        reverse: false,      // 称呼方式：true对方称呼我,false我称呼对方
+        mode: 'default',     // 模式选择：使用setMode方法定制不同地区模式，在此选择自定义模式
+        optimal: false       // 最短关系：计算两者之间的最短关系
+    }, parameter);
     // 切换模式
     getModeData(options.mode);
-
-    let from_selectors = getSelectors(options.text);
-    let to_selectors = getSelectors(options.target);
-    if(!to_selectors.length){
-        to_selectors = [''];
+    let fromSelectors = getSelectors(options.text);
+    let toSelectors = getSelectors(options.target);
+    if (!toSelectors.length) {
+        toSelectors = [''];
     }
-    let result = [];                            //匹配结果
-    // console.log('[selectors]',from_selectors,to_selectors);
-    from_selectors.forEach(function(from_selector){
-        to_selectors.forEach(function(to_selector){
-            mergeSelector({
-                from:from_selector,
-                to:to_selector,
-                sex:options.sex,
-                optimal:options.optimal
-            }).forEach(function(data){
-                // console.log('[data]',from_selector,to_selector,options.optimal,data);
-                let ids = data?selector2id(data['selector'],data['sex']):[];
-                // console.log('[ids]',data['selector'],data['sex'],ids);
-                ids.forEach(function(id){
+    const result = fromSelectors.flatMap(fromSelector => {
+        return toSelectors.flatMap(toSelector => {
+            return mergeSelector({
+                from: fromSelector,
+                to: toSelector,
+                sex: options.sex,
+                optimal: options.optimal
+            }).flatMap(data => {
+                const ids = data ? selector2id(data['selector'], data['sex']) : [];
+                return ids.flatMap(id => {
                     let temps = [id];
                     let sex = data['sex'];
-                    if(options.reverse){
+                    if (options.reverse) {
                         temps = reverseId(id,sex);
                         if(id.match(/([fhs1](&[ol\d]+)?|[olx]b)$/)){
                             sex = 1;
@@ -49,33 +44,25 @@ let relationship = function (parameter){
                             sex = 0;
                         }
                     }
-                    if(options.type=='chain'){
-                        temps.forEach(function(id){
-                            let item = getChainById(id,data['sex']);
-                            if(item){
-                                result.push(item);
-                            }
-                        });
-                    }else if(options.type=='pair'){
-                        temps = reverseId(id,data['sex']);
-                        temps.forEach(function(r_id){
-                            let pairs = getPairsById(id,r_id);
-                            result = result.concat(pairs);
-                        });
-                    }else{
-                        temps.forEach(function(id){
+                    if (options.type === 'chain') {
+                        return temps.map(id => getChainById(id, sex)).filter(item => item);
+                    } else if (options.type === 'pair') {
+                        const reversedTemps = reverseId(id, data['sex']);
+                        return reversedTemps.flatMap(rId => getPairsById(id, rId));
+                    } else {
+                        return temps.flatMap(id => {
                             let items = getItemsById(id);
-                            if(!items.length){
-                                items = getItemsById(sex+','+id);
+                            if (!items.length) {
+                                items = getItemsById(sex + ',' + id);
                             }
-                            result = result.concat(items);
+                            return items;
                         });
                     }
                 });
             });
         });
     });
-    // console.log('[result]',result);
+
     return [...new Set(result)];
 };
 
