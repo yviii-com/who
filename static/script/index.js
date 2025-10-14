f71359e3214b72d12d3d8aeb12a9fcac8451fa33
mumuy <zhenghaole1990@126.com>
2025-10-14 23:40:05 +0800

    updated 统一风格，和主页保持一致

--- static/script/index.js
+++ static/script/index.js
@@ -1,14 +1,3 @@
-// 头部滚动
-(function(){
-    let $header = document.querySelector('.mod-head');
-    document.querySelector('.scroll-container').addEventListener('scroll',function(){
-        let scrollTop = this.scrollTop;
-        let ratio = Math.min(scrollTop/1000,1);
-        $header.style.background = 'rgba(255,255,255,'+(ratio*0.8)+')';
-        $header.style.boxShadow = '1px 1px 6px rgba(0,0,0,'+(ratio*0.1)+')';
-    });
-})();
-
 // 特征选项卡
 (function(){
     let $mod_feature = document.querySelector('.mod-feature');
@@ -30,17 +19,4 @@
         });
     });
     togglePanel(0);
-})();
-
-// 底部滚动
-(function(){
-    let $gotop = document.querySelector('.mod-fixedbar .gotop');
-    let $container = document.querySelector('.scroll-container');
-    $gotop.addEventListener('click',function(){
-        $container.scrollTo({
-            left:0,
-            top:0,
-            behavior:'smooth'
-        });
-    });
 })();
